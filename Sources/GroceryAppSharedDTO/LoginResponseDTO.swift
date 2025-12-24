95422baf0e248a8ce026de0c0f0820639f0b1c6b
rgago <rugana90@gmail.com>
2025-12-24 09:38:03 +0100

    Add DTOs and make them public

---
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterResponseDTO.swift
+//  GroceryApp
+//
+//  Created by Rubén Gago on 23/12/25.
+//
+
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+  public let error: Bool
+  public var reason: String? = nil
+  public var token: String? = nil
+  public var userId: UUID? = nil
+  
+  public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+    self.error = error
+    self.reason = reason
+    self.token = token
+    self.userId = userId
+  }
+}
---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterResponseDTO.swift
+//  GroceryApp
+//
+//  Created by Rubén Gago on 23/12/25.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+  public let error: Bool
+  public var reason: String? = nil
+  
+  public init(error: Bool, reason: String? = nil) {
+    self.error = error
+    self.reason = reason
+  }
+}
